3048bbfb2a81af2f690a55150127669255a1fbce
Stefan Scheler <stefan@scheler.com>
2014-02-08 19:50:47 +0100

    added windows debugging code

--- src/ui/consoletab.cpp
+++ src/ui/consoletab.cpp
@@ -9,6 +9,9 @@
 #include <QMessageBox>
 #include <QFontDialog>
 #include <QSettings>
+#if defined(Q_OS_WIN)
+#include <windows.h>
+#endif
 
 #include "consoletab.h"
 #include "ui_consoletab.h"
@@ -21,6 +24,46 @@ Q_DECLARE_METATYPE(QSerialPort::DataBits)
 Q_DECLARE_METATYPE(QSerialPort::StopBits)
 Q_DECLARE_METATYPE(QSerialPort::Parity)
 Q_DECLARE_METATYPE(QSerialPort::FlowControl)
+
+void dumpDCB(const char *strFileName)
+{
+#if defined(Q_OS_WIN)
+    HANDLE h = CreateFileA(strFileName, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, OPEN_EXISTING,  FILE_ATTRIBUTE_NORMAL, 0);
+
+    DCB dcb;
+
+    GetCommState(h, &dcb);
+
+    printf("dcb.DCBlength:         0x%08x\n", dcb.DCBlength);
+    printf("dcb.BaudRate:          0x%08x (%lu)\n", dcb.BaudRate, dcb.BaudRate);
+    printf("dcb.fBinary:           0x%08x\n", dcb.fBinary);
+    printf("dcb.fParity:           0x%08x\n", dcb.fParity);
+    printf("dcb.fOutxCtsFlow:      0x%08x\n", dcb.fOutxCtsFlow);
+    printf("dcb.fOutxDsrFlow:      0x%08x\n", dcb.fOutxDsrFlow);
+    printf("dcb.fDtrControl:       0x%08x\n", dcb.fDtrControl);
+    printf("dcb.fDsrSensitivity:   0x%08x\n", dcb.fDsrSensitivity);
+    printf("dcb.fTXContinueOnXoff: 0x%08x\n", dcb.fTXContinueOnXoff);
+    printf("dcb.fOutX:             0x%08x\n", dcb.fOutX);
+    printf("dcb.fInX:              0x%08x\n", dcb.fInX);
+    printf("dcb.fErrorChar:        0x%08x\n", dcb.fErrorChar);
+    printf("dcb.fNull:             0x%08x\n", dcb.fNull);
+    printf("dcb.fRtsControl:       0x%08x\n", dcb.fRtsControl);
+    printf("dcb.fAbortOnError:     0x%08x\n", dcb.fAbortOnError);
+    printf("dcb.XonLim:            0x%04x\n", dcb.XonLim);
+    printf("dcb.XoffLim:           0x%04x\n", dcb.XoffLim);
+    printf("dcb.ByteSize:          0x%02x\n", dcb.ByteSize);
+    printf("dcb.Parity:            0x%02x\n", dcb.Parity);
+    printf("dcb.StopBits:          0x%02x\n", dcb.StopBits);
+
+    printf("dcb.XonChar:           0x%02x\n", dcb.XonChar);
+    printf("dcb.XoffChar:          0x%02x\n", dcb.XoffChar);
+    printf("dcb.ErrorChar:         0x%02x\n", dcb.ErrorChar);
+    printf("dcb.EofChar:           0x%02x\n", dcb.EofChar);
+    printf("dcb.EvtChar:           0x%02x\n", dcb.EvtChar);
+
+    CloseHandle(h);
+#endif
+}
 
 ConsoleTab::ConsoleTab(ConsoleTabWidget *parent) :
     QWidget(parent),
@@ -193,6 +236,10 @@ void ConsoleTab::onConnectClicked(void)
     if (!m_port)
     {
         m_port = new QSerialPort(portName);
+
+        m_port->setSettingsRestoredOnClose(false);
+
+        dumpDCB(portName.toStdString().c_str());
 
         if (m_port->open(QIODevice::ReadWrite))
         {
