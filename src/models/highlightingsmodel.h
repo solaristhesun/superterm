f1e018ea9c8aa4f741e37aa0ccb428ea98ae419c
Stefan Scheler <stefan@scheler.com>
2018-10-03 19:51:17 +0200

    code cleanup

--- src/models/highlightingsmodel.h
+++ src/models/highlightingsmodel.h
@@ -14,12 +14,12 @@ public:
     int      rowCount(const QModelIndex& parent = QModelIndex()) const override;
     QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
 
-    void setHighlightings(QList<Highlighting> highlightings);
-    void addHighlighting(Highlighting highlighting);
+    void                setHighlightings(QList<Highlighting> highlightings);
+    void                addHighlighting(Highlighting highlighting);
     QList<Highlighting> highlightings() const;
-    void remove(const QModelIndex& index);
-    void removeAll();
-    QColor lineColor(QString line);
+    void                remove(const QModelIndex& index);
+    void                removeAll();
+    QColor              lineColor(QString line);
 
 signals:
     void highlightingChanged();
